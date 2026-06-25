cd77135e3ce97c06b20231ce549116a69db3d590
Doan <bui00083@algonquinlive.com>
2026-06-25 19:32:15 -0400

    Completes the assignment.

--- t-shirts.js
+++ t-shirts.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
 const tshirts = [
   {
     title: 'Blue T-Shirt',
@@ -62,4 +64,67 @@ const tshirts = [
     stock: 2,
     quantity: 1
   }
-]
+];
+
+function App() {
+  const [inventory, setInventory] = useState(tshirts);
+
+  const [quantity, setQuantity] = useState({});
+
+  function handleQuantityChange(index, value) {
+    setQuantity({
+      ...quantity,
+      [index]: Number(value)
+    });
+  }
+
+  function handleStock(index) {
+    const selectedQty = quantity[index] || 1;
+
+    const updatedTshirts = inventory.map((product, i) => {
+      if (i === index) {
+        return { ...product, stock: product.stock - selectedQty };
+      }
+      return product;
+    });
+    setInventory(updatedTshirts);
+    setQuantity({ ...quantity, [index]: 1 });
+  }
+
+  return (
+    <div class="container">
+      <h1>T-shirts</h1>
+
+      <div class="items">
+        {inventory.map((product, index) => (
+          <div class="item" key={index}>
+            <img src={`./images/${product.image}`}/>
+            <h2>{product.title}</h2>
+            <p class="price">${product.price}</p>
+            {product.stock > 0
+              ? <p class="alert ">{product.stock} left!</p>
+              : <p class="alert">Out of stock!</p>
+            }
+            {product.stock > 0 && (
+              <>
+                <select
+                  value={quantity[index] || 1}
+                  onChange={(e) => handleQuantityChange(index, e.target.value)}
+                >
+                  {Array.from({ length: product.stock }, (_, i) => i + 1).map(num => (
+                    <option key={num} value={num}>{num}</option>
+                  ))}
+                </select>
+
+                <button onClick={() => handleStock(index)}>Buy</button>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
